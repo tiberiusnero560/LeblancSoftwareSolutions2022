6c7fd67838721431384709d01b17f14f083f4365
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2022-06-04 23:04:13 -0400

    minor progress

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styles from '../styles/footer.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../public/logo/LSS-logo.jpg'
 
 const Footer = () => {
   return (
@@ -12,21 +15,47 @@ const Footer = () => {
                 </div>
 
                 <div className={styles.col}>
-                    <h3>About</h3>
+                    <h3>Services</h3>
 
                 </div>
 
                 <div className={styles.col}>
-                    <h3>About</h3>
+                    <h3>Our Clients</h3>
 
                 </div>
 
                 <div className={styles.col}>
-                    <h3>About</h3>
+                    <h3>Contact Us</h3>
 
+                </div>
+
+                    {/* Logo container */}
+                {/* <div className={styles.logoWrapper}>
+                    <div className={styles.logoContainer}>
+                        <Link href="/">
+                            <Image
+                                src={logo}
+                                
+                                className={styles.image}
+                            />
+                        </Link>
+
+                    </div>
+                </div> */}
+                    
+                <div className={styles.container2}>
+                    <div className={styles.row2}>
+                        <div className={styles.col2}>
+                            <h3>Jobs</h3>
+                            <h3>IT Consulting</h3>
+                            <h3>About</h3>
+
+                        </div>
+                    </div>
                 </div>
 
             </div>
+
         </div>
     </div>
   )
