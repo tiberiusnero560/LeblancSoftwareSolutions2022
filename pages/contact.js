ab1f640d6969762ff89162aef1ab88b946f7cb7e
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2022-06-14 19:46:54 -0400

    react form hook

--- pages/contact.js
+++ pages/contact.js
@@ -1,93 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 import styles from '../styles/contact.module.css'
 
 const contact = () => {
-    const sendMail = async (data) => {
-
-        try {
-          await fetch("/api/contact", {
-            "method": "POST",
-            "headers": { "content-type": "application/json" },
-            "body": JSON.stringify(data)
-          })
-    
-        } catch (error) {
-            console.log(error);
-        }
-    
-      }
-    
-
-  return (
-    <div className={styles.pageWrapper}>
-        <div className={styles.titleContainer}>
-            <h1 className={styles.title}>Contact</h1>
-
-        </div>
-
-        <div className={styles.formWrapper}>
-            <div className={styles.formContainer}>
-                <form onSubmit={sendMail} className={styles.form}>
-                    <div className={styles.formItem}>
-                        <div className={styles.formItemContent}>
-                            <label className={styles.label}>Name</label>
-                            <input className={styles.input} 
-                                type="text" 
-                                placeholder="Enter your name" 
-                                name="name"
-                                required
-                            />
-                        </div>
-                    </div>
-                    
-                     <div className={styles.formItem}>
-                        <div className={styles.formItemContent}>
-                            <label className={styles.label}>Subject</label>
-                            <input className={styles.input} 
-                                type="text" 
-                                placeholder="Enter your subject"
-                                name="subject"
-                                required
-                            />
-                        </div>
-                    </div>
-                    
-                     <div className={styles.formItem}>
-                        <div className={styles.formItemContent}>
-                            <label className={styles.label}>Company</label>
-                            <input className={styles.input} 
-                                type="text"
-                                placeholder="Enter name of your company"
-                                name="company"
-                                optional
-                            />
-                        </div>
-                    </div>
-
-                     <div className={styles.formItem}>
-                        <div className={styles.formItemTextArea}>
-                            <label className={styles.label}>Content</label>
-                            <textarea className={styles.textarea}
-                                placeholder="Enter your message"
-                                name="content"
-                                required
-                            >
-
-                            </textarea>
-
-                        </div>
-                    </div>
-
-                    <div>
-                        <button className={styles.submit} type="submit">Submit</button>
-                    </div>
-
-
-                </form>
-            </div>   
-        </div>
-    </div>
-  )
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+  
+    console.log(watch("example")); // watch input value by passing the name of it
+  
+    return (
+      /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+        
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+        
+        <input type="submit" />
+      </form>
+    );
 }
 
 export default contact
