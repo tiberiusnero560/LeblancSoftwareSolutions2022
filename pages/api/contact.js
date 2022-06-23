882a690b7511bd798f2ba0374dfabef0be01ca08
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2022-06-23 17:47:00 -0400

    fix error

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -6,12 +6,12 @@ const sgMail = require('@sendgrid/mail')
   // using Twilio SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
   const msg = {
-    to: 'kyle.leblanc88@gmail.com', // Change to your recipient
-    from: 'kyle_leblanc16@hotmail.com', // Change to your verified sender
-    subject: ${req.body.subject},
+    to: 'kyle.leblanc88@gmail.com', 
+    from: 'kyle_leblanc16@hotmail.com', 
+    subject: {req.body.subject},
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  },
+  }
   sgMail
     .send(msg)
     .then(() => {
