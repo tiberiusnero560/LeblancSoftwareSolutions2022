657a8fa201addad2c1904588ec0fce874d52e94e
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2022-06-26 21:54:31 -0400

    Adding nextjs specific code
    Adding the code needed for nextjs. Adding the handlers to the _app and _document files to inject the google code into the bundle

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
 import Layout from '../components/layout'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 
 function MyApp({ Component, pageProps }) {
+    const router = useRouter()  
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            ga.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
   return (
       <Layout>
             <Component {...pageProps} />   
