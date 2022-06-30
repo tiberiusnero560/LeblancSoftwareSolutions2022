4741eed4ce01ab72be7eef2593026297752ff517
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2022-06-29 20:07:51 -0400

    tweak

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,26 @@ import '../styles/globals.css'
 import Layout from '../components/layout'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { getLocationOrigin } from 'next/dist/shared/lib/utils'
 
 
 function MyApp({ Component, pageProps }) {
     const router = useRouter()  
-
+    
     useEffect(() => {
         const handleRouteChange = (url) => {
             ga.pageview(url)
+            
         }
+        document.addEventListener( 'wpcf7mailsent', function( event ) {
+            ga( 'send', 'event', 'Contact Form', 'submit' );
+        }, false );
         router.events.on('routeChangeComplete', handleRouteChange)
         return () => {
             router.events.off('routeChangeComplete', handleRouteChange)
         }
     }, [router.events])
 
-    document.addEventListener( 'wpcf7mailsent', function( event ) {
-        ga( 'send', 'event', 'Contact Form', 'submit' );
-    }, false );
 
   return (
       <Layout>
